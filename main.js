ec6fbae537ac75dac63259b74bcc55ae30b30156
ZombGuy <zombguy1234@gmail.com>
2017-09-15 19:40:04 -0500

    added moopers

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ function moopClick(number){
     Moops = Moops + number;
 	document.getElementById("Moops").innerHTML = Moops;
 };
+
+var moopers = 0;
+function buyMooper(){
+	var mooperCost = Math.floor(10 * Math.pow(1.1,moopers));
+	if(Moops >= mooperCost){
+		moopers = moopers + 1;
+		Moops = Moops - mooperCost;
+		document.getElementById('moopers').innerHTML = moopers
+		document.getElementById('Moops').innerHTML = Moops
+	};
+	var nextCost = Math.floor(10 * Math.pow(1.1,moopers));
+	document.getElementById('mooperCost').innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+
+moopClick(moopers);
+
+
+}, 1000);
